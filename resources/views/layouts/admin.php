26f255dc180b0fcc12fea5762799e8c66b9b6889
Sandra Espinoza <san.espinoz@gmail.com>
2016-08-04 15:21:11 -0300

    Se modifico el link inicio

--- resources/views/layouts/admin.php
+++ resources/views/layouts/admin.php
@@ -35,7 +35,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href='/'>Inicio</a>
+                <a class="navbar-brand" href='/bms2/public/'>Inicio</a>
             </div>
            
 
