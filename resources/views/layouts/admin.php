df2cbd8b55485c43165ec676440203130f5014a8
Sandra Espinoza <san.espinoz@gmail.com>
2016-08-04 16:51:58 -0300

    Modificado el link para el logout

--- resources/views/layouts/admin.php
+++ resources/views/layouts/admin.php
@@ -29,7 +29,7 @@
         
         <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -40,20 +40,17 @@
            
 
             <ul class="nav navbar-top-links navbar-right">
-                 <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                    <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a ng-click="logout()" href='/bms2/public/'>
+                        Logout
                     </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        
-                       
-                       <li ng-show="isLogged">
-                            <a ng-click="logout()" href>Logout</a>
-                        </li>
-                    </ul>
+                   
+                    <!-- /.dropdown-user -->
                 </li>
+                <!-- /.dropdown -->
             </ul>
-
+            <!-- /.navbar-top-links -->
             <div class="navbar-default sidebar" role="navigation"  ng-controller='pisoCtrl'>
                 <div class="sidebar-nav navbar-collapse">
                      <ul class="nav" id="side-menu">
