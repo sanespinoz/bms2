c3b842476d85bc4665ee0dd479e229ff5846bdc9
Sandra Espinoza <san.espinoz@gmail.com>
2016-08-05 17:21:09 -0300

    agregue libreria para material design bootstrap

--- resources/views/index.php
+++ resources/views/index.php
@@ -9,9 +9,9 @@
 
         <title>SAI</title>
 
-        <!-- Bootstrap Core CSS -->
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
-
+   
+        <!-- Bootstrap -->
+        <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <!-- Custom CSS -->
         <link href="css/grayscale.css" rel="stylesheet">
 
@@ -20,6 +20,9 @@
         <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
         <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 
+        <  <!-- Material Design fonts -->
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
@@ -27,6 +30,9 @@
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
 
+        <!-- Bootstrap Material Design -->
+        <link rel="stylesheet" type="text/css" href="dist/css/bootstrap-material-design.css">
+        <link rel="stylesheet" type="text/css" href="dist/css/ripples.min.css">
     </head>
 
     <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top" ng-controller="AppCtrl">
